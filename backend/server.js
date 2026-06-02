3f7aee3196855f3bce335e82e0f29d84580f313b
Hemanth Krishna <dandamudihk@gmail.com>
2026-06-02 21:45:15 +0530

    Configure dynamic port for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const translationRouter = require('./src/routes/translationRouter');
 const ttsRouter = require('./src/routes/ttsRouter');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
